907f0e05b7a63050702d5804387c7ed8ca6efa61
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-10-01 11:08:55 -0300

    Adicionando funcionalidades do JavaScript
    Signed-off-by: srmatheusmaciel <srmatheusmaciel@hotmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif']
+
+
+    },
+
+
     extend: {
       backgroundImage:{
       "home": "url('/assets/bg.png')",
