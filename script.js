1acb73863ded148d2d1683bb595712399f7c0337
srmatheusmaciel <srmatheusmaciel@hotmail.com>
2024-10-01 10:53:10 -0300

    feat: Adicionando JS no projeto.
    Criei a pasta script.js pra adicionar algumas funcionalidades ao projeto.

---
+++ script.js
@@ -0,0 +1,156 @@
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+
+
+let cart = [];
+
+//abrir o modal do carrinho
+cartBtn.addEventListener("click", function () {
+  updateCartModal();
+  cartModal.style.display = "flex"
+  
+});
+
+//fechar o modal quando clicar fora
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+});
+
+//fechar o modal quando clicar em "fechar"
+closeModalBtn.addEventListener("click", function (event) {
+  cartModal.style.display = "none";
+});
+
+//Pegar o evento ao clicar no carrinho e inserir nome e preço
+menu.addEventListener("click", function (event) {
+  let parentButton = event.target.closest(".add-to-cart-btn");
+
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
+    addToCart(name, price);
+  }
+
+});
+
+//função para adicionar no carrinho
+function addToCart(name, price) {
+  const existingItem = cart.find(item => item.name === name);
+
+  if (existingItem) {
+    //Se existir o item, aumentará a quantidade apenas
+    existingItem.quantity += 1;
+
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+
+    })
+  };
+
+  updateCartModal();
+
+};
+
+
+//atualizar carrinho
+function updateCartModal() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach(item => {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
+    cartItemElement.innerHTML = `
+      <div class="flex items-center justify-between">
+        <div>
+          <p class="font-medium">${item.name}</p>
+          <p>Qtd: ${item.quantity}</p>
+          <p class="font-medium mt-2">${item.price.toFixed(2)}</p>
+        </div>
+
+        <div>
+          <button class="text-red-500 hover:text-red-700 remove-from-cart-btn" data-name="${item.name}">
+           Remover  
+          <button> 
+        </div>
+      </div>  
+    
+    `
+      total += item.price * item.quantity;
+    cartItemsContainer.appendChild(cartItemElement);
+  })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+
+  });
+
+    cartCounter.innerHTML = cart.length;
+
+}
+
+
+//Remover item do carrinho
+cartItemsContainer.addEventListener("click", function (event) {
+  if(event.target.classList.contains("remove-from-cart-btn")){
+    const name = event.target.getAttribute("data-name");
+
+    removeItemCart(name);
+  }
+})
+
+function removeItemCart(name){
+  const index = cart.findIndex(item => item.name ===name);
+
+  if(index !== -1){
+    const item = cart[index];
+
+    if(item.quantity > 1){
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+
+    cart.splice(index,1);
+    updateCartModal();
+
+  }
+
+}
+
+
+//Salvar endereço
+
+addressInput.addEventListener("input", function(event){
+
+  let inputValue = event.target.value;
+
+  //
+
+})
+
+//finalizar o carrinho
+checkoutBtn.addEventListener("click", function(){
+  if(cart.length === 0) return;
+  if(addressInput.value === ""){
+    addressWarn.classList.remove("hidden");
+    addressInput.classList.add("border-red-500");
+
+
+  }
+
+})
